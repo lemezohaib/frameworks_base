77785b149b469853ff20578fae7bf3086d347242
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-24 22:38:00 +0530

    PixelPropsUtils: Allow spoofing device model for Netflix
    It can be used to unlock HDR for devices that don't
    have it by default.
    
    Set config_netflixSpoofModel to the desired model in
    device overlay.
    
    Change-Id: Ibfa3c590ef2b388e18de2bb152114291623143b9
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -18,9 +18,12 @@
 package com.android.internal.util.custom;
 
 import android.app.Application;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.SystemProperties;
 import android.util.Log;
+
+import com.android.internal.R;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -36,6 +39,7 @@ public class PixelPropsUtils {
     private static final boolean DEBUG = false;
 
     private static final String SAMSUNG = "com.samsung.android.";
+    private static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
 
     private static final Map<String, Object> propsToChange;
     private static final Map<String, Object> propsToChangePixel5;
@@ -100,6 +104,9 @@ public class PixelPropsUtils {
     private static volatile boolean sIsGms = false;
     private static volatile boolean sIsFinsky = false;
 
+    private static final String sNetflixModel =
+            Resources.getSystem().getString(R.string.config_netflixSpoofModel);
+
     static {
         propsToKeep = new HashMap<>();
         propsToChange = new HashMap<>();
@@ -145,6 +152,9 @@ public class PixelPropsUtils {
             } else if (packageName.equals("com.android.vending")) {
                 sIsFinsky = true;
                 return;
+            } else if (!sNetflixModel.isEmpty() && packageName.equals(PACKAGE_NETFLIX)) {
+                dlog("Setting model to " + sNetflixModel + " for Netflix");
+                setPropValue("MODEL", sNetflixModel);
             } else if (!isPixelDevice) {
                 if ((Arrays.asList(packagesToChangePixel7Pro).contains(packageName))) {
                     propsToChange.putAll(propsToChangePixel7Pro);
@@ -220,5 +230,9 @@ public class PixelPropsUtils {
             Log.i(TAG, "Blocked key attestation sIsGms=" + sIsGms + " sIsFinsky=" + sIsFinsky);
             throw new UnsupportedOperationException();
         }
+    }
+
+    private static void dlog(String msg) {
+      if (DEBUG) Log.d(TAG, msg);
     }
 }
